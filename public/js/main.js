9fcfb76654e89722fae81a1befe7a27089e3fc5c
Andres Riofrio <riofrios@gmail.com>
2012-12-13 23:38:03 -0800

    Include multiple days and snap

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,20 @@
 
 var container = document.getElementById("container");
 
-function addReadings(now) {
-  var date = now.getFullYear() + "-" +
-             ("0"+(now.getMonth()+1)).slice(-2) + "-" +
-             ("0"+now.getDate()).slice(-2);
-  var dateString = now.toLocaleDateString();
+function addReadings(date, referenceElement, callback) {
+  var isoDate = date.getFullYear() + "-" +
+             ("0"+(date.getMonth()+1)).slice(-2) + "-" +
+             ("0"+date.getDate()).slice(-2);
+  var humanDate = date.toLocaleDateString();
 
   var item = document.createElement("article");
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/api/" + date, true);
+  xhr.open("GET", "/api/" + isoDate, true);
   xhr.onload = function() {
     var data = JSON.parse(xhr.responseText);
 
     var h2 = document.createElement("h2");
-    h2.textContent = dateString;
+    h2.textContent = humanDate;
     item.appendChild(h2);
 
     var h1 = document.createElement("h1");
@@ -32,16 +32,75 @@ function addReadings(now) {
     }
     item.appendChild(dl);
 
-    container.appendChild(item);
+    container.insertBefore(item, referenceElement);
+    if(callback) callback(item);
   };
   xhr.send();
 }
 
-var date = new Date();
-date.setDate(date.getDate() - 5);
-addReadings(date);
-for(var i=0; i<11; i++) {
-  date.setDate(date.getDate() + 1);
-  addReadings(date);
+function addReadingsBefore(date, dateElement, count, cb) {
+  date.setDate(date.getDate() - 1);
+  addReadings(date, dateElement, function(dateElement) {
+    window.scrollTo(window.scrollX + dateElement.offsetWidth, window.scrollY);
+    if(cb) cb(dateElement, count);
+    if(count > 1)
+      addReadingsBefore(date, dateElement, count - 1, cb);
+  });
 }
+function addReadingsAfter(date, dateElement, count, cb) {
+  date.setDate(date.getDate() + 1);
+  addReadings(date, dateElement.nextSibling,
+      function(dateElement) {
+    if(cb) cb(dateElement, count);
+    if(count > 1)
+      addReadingsAfter(date, dateElement, count - 1, cb);
+  });
+}
+
+function scrollIntoView(element) {
+  window.scrollTo(element.offsetLeft, window.scrollY);
+}
+
+addReadings(new Date(), null, function(todayElement) {
+  addReadingsBefore(new Date(), todayElement, 5);
+  addReadingsAfter(new Date(), todayElement, 5);
+});
+
+var debounce = function(func, wait, immediate) {
+  var timeout, result;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) result = func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) result = func.apply(context, args);
+    return result;
+  };
+};
+
+(function() {
+  var adjust = 1;
+  if(parseInt(navigator.userAgent.match(/Chrom(e|ium|eframe)\/([0-9]+)\./i)[2]) <= 18) { // http://crbug.com/141840
+    adjust = window.devicePixelRatio;
+  }
+
+  var snapOnScroll = debounce(function() {
+    console.log("snapping");
+    var element = document.elementFromPoint(
+      adjust * document.body.clientWidth/2, 0);
+
+    container.style.transform = "translateX(" +
+      (window.scrollX - element.offsetLeft) + "px)";
+    container.style.webkitTransform = 
+      container.style.mozTransform = 
+      container.style.msTransform = 
+      container.style.oTransform = container.style.transform;
+  }, 50);
+
+  window.addEventListener("scroll", snapOnScroll);
+})();
 
